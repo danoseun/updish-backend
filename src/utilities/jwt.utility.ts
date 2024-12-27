f1bb70b971120edc75f616fde206f38ea86a5dee
danoseun <ayodejisomefun@yahoo.com>
2024-12-27 12:47:22 +0100

    debugging jwt issues

--- src/utilities/jwt.utility.ts
+++ src/utilities/jwt.utility.ts
@@ -1,12 +1,16 @@
+import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
 import { User } from '../interfaces';
 
 import variables from '../variables';
 
+dotenv.config();
+
 export class JWT {
   private static secret: jwt.Secret = variables.auth.secret;
 
   public static encode(payload: Partial<User>, options?: Partial<jwt.SignOptions>): string {
+    console.log('secret', this.secret, '', process.env.JWT_SECRET);
     try {
       const token = jwt.sign(payload, this.secret, { expiresIn: variables.auth.jwtExpiryTime || '30d', ...options });
       return token;
