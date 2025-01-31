bfd0501bcadca3ea6b353c2854ff1368763ebb12
danoseun <ayodejisomefun@yahoo.com>
2025-01-31 19:17:44 +0100

    fixing import issues

--- src/controllers/webhook.ts
+++ src/controllers/webhook.ts
@@ -2,11 +2,11 @@ import { RequestHandler } from 'express';
 import HttpStatus from 'http-status-codes';
 import pool from '../config/database.config';
 import crypto from 'crypto';
-import { generateRandomCode, logger, respond } from '@src/utilities';
+import { generateRandomCode, logger, respond } from '../utilities';
 import { getLastOrderService } from './order';
-import { sql } from '@src/database/sql';
-import { verifyPayment } from '@src/services/flutterwave';
-import { ORDER_STATUS } from '@src/constants';
+import { sql } from '../database/sql';
+import { verifyPayment } from '../services/flutterwave';
+import { ORDER_STATUS } from '../constants';
 
 interface WebhookData {
   tx_ref: string;
